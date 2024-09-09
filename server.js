c33746110ebd210fc27982bb952bd95e2f5b3f40
ghadeer <ghadeer1781@gmail.com>
2024-09-10 00:46:52 +0300

    set up a basic Express server to handle routing and connect your back-end with the front-end

---
+++ server.js
@@ -0,0 +1,25 @@
+// Import required modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create an Express app
+const app = express();
+const port = 3000; // You can change the port if needed
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Serve static files from the "views" directory
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Basic route to render index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
