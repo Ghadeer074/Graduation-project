7f8b59e4b8768e579bfccd7144e07c296bf73a63
ghadeer <ghadeer1781@gmail.com>
2024-09-11 06:10:45 +0300

    1

--- public/script.js
+++ public/script.js
@@ -1,1 +1,13 @@
 // JavaScript front-end codes here 
+document.querySelector('#content').innerHTML = `<button id="sendData">Send Data</button>`;
+
+document.querySelector('#sendData').addEventListener('click', () => {
+    fetch('/api/data', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: 'Sample', value: 42 })
+    })
+    .then(response => response.json())
+    .then(data => console.log('Data sent to server:', data))
+    .catch(error => console.error('Error:', error));
+});
--- server.js
+++ server.js
@@ -23,3 +23,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+const Data = require('./models/data');
+
+app.post('/api/data', (req, res) => {
+    const newData = new Data(req.body);
+
+    newData.save()
+        .then(result => res.json(result))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
