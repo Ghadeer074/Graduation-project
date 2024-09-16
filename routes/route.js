212470d16a38688e070d6046468f319104c0069d
ghadeer <ghadeer1781@gmail.com>
2024-09-16 11:32:01 +0300

    comments

--- routes/route.js
+++ routes/route.js
@@ -1,3 +1,13 @@
 //app.get('/',(req,res)=> { 
  //res.sendFile("./views/index.html")
 //})
+
+//const express = require("express");
+//const router = express.Router();
+//const  = require("../controllers/userController");
+
+
+//router.get('/', );
+//router.post('/', .createPost);
+
+//module.exports = router;
