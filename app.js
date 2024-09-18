afc7e5b5cc66db23b68455ee1628a58381167cde
ghadeer <ghadeer1781@gmail.com>
2024-09-18 14:14:53 +0300

    Database connection

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 
 // Create an Express app
 const app = express();
-const port = 3000; 
+const port = 2002; 
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the "views" directory
-//app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // Serve static files (CSS, JS) from the 'public' folder
 app.use(express.static('public'));
@@ -42,11 +43,13 @@ app.get('/', (req, res) => {
 });
 
 
-// Start the server
-app.listen(port, () => {
+// Start the server + database
+mongoose.connect("mongodb+srv://ghadeer:0iuDyICJDPAKxGur@cluster0.ifqxq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+.then(() => {app.listen(port, () => {
     console.log(`http://localhost:${port}`);
 });
-
+})
+.catch((err) => {console.log(err)});
 //const Data = require('./models/data');
 
 //app.post('/api/data', (req, res) => {
@@ -56,4 +59,9 @@ app.listen(port, () => {
   //      .then(result => res.json(result))
       //  .catch(err => res.status(400).json('Error: ' + err));
 //});
+
+//// Routes
+//app.get('/', (req, res) => {
+  //  res.send('Welcome to the App!');
+//  ^});
 
