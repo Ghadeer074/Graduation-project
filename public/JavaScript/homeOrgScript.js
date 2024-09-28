ae867f2d9ac3da76a8ae13f9a782f6c3230450cf
Jana-Alzaidi <jana.alzaidi@hotmail.com>
2024-09-28 07:02:59 +0300

    add css,js for home page

---
+++ public/JavaScript/homeOrgScript.js
@@ -0,0 +1,15 @@
+//  to Tracking page
+document.getElementById("trackingButton").onclick = function() {
+    location.href = 'tracking.html';
+};
+
+//  to Pilgrim Group page
+document.getElementById("pilgrimGroupButton").onclick = function() {
+    location.href = 'pilgrim-group.html';
+};
+
+//  to Chat with Pilgrims page
+document.getElementById("chatButton").onclick = function() {
+    location.href = 'chat.html';
+};
+   
