6bf22f364194c1b6840f6a504a1003ab7962dc96
ghadeer <ghadeer1781@gmail.com>
2024-10-05 11:11:26 +0300

    update

--- controllers/controler-landing.js
+++ controllers/controler-landing.js
@@ -1,7 +1,4 @@
-// basic starting file will be modified later 
-
 //Handle a GET request for index.ejs (Landing page)
-//const landingPage = require("");
 const landing = (req, res) => {
     res.render("index",{});
 };
